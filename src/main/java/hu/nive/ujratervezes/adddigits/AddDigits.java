5668d58cc6aeeb4f1027dab3ba68243de0cccd6f
Luca Zibrinyi <zvmluci@gmail.com>
2021-04-29 12:01:05 +0200

    AddDigits

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,19 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    public int addDigits(String input) {
+        int result = 0;
+        if (input == null || input.length() == 0) {
+            return -1;
+        
+        } else {
+            for (char c : input.replaceAll("\\D", "").toCharArray()) {
+                int digit = c - '0';
+                result += digit;
+
+            }
+            return result;
+        }
+
+    }
 }
